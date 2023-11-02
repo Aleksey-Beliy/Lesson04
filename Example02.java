2de27f09d5895a7a4525920179c9d566ba959f96
Aleksey-Beliy <148489949+Aleksey-Beliy@users.noreply.github.com>
2023-11-02 19:12:47 +0300

    Add files via upload

---
+++ Example02.java
@@ -0,0 +1,36 @@
+package edu.academy.it.lesson04;
+
+public class Example02 {
+
+	public static void main(String[] args) {
+		double a = 10, b = 8;
+		double x = 9, y = 9, z = 20;
+		if (a > x) {
+			if (b > y)
+				System.out.println("Проидет.");
+		}
+		if (a > y) {
+			if (b > x)
+				System.out.println("Проидет.");
+		}
+		if (a > x) {
+			if (b > z)
+				System.out.println("Проидет.");
+		}
+		if (a > z) {
+			if (b > x)
+				System.out.println("Проидет.");
+		}
+		if (a > y) {
+			if (b > z)
+				System.out.println("Проидет.");
+		}
+		if (a > z) {
+			if (b > y)
+				System.out.println("Проидет.");
+		} else {
+			System.out.println("Не проидет.");
+		}
+
+	}
+}
